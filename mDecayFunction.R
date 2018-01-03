be2eeb4c284bbaec093fdf59167808f04fd3c6b5
Paul Frater <pnfrater@gmail.com>
2018-01-03 09:49:58 -0600

    Modified mDecayFunction.R

--- mDecayFunction.R
+++ mDecayFunction.R
@@ -1,16 +1,16 @@
 # natural mortality as a function of age with functions to optimize such
-m.decay.func <- function(age, m.decay, max.m, min.m) {
+m_decay_func <- function(age, m.decay, max.m, min.m) {
     exp((-1)*m.decay*age)*(max.m - min.m) + min.m
 }
 
-m.decay.optimizer <- function(data, age) {
+m_decay_optimizer <- function(data, age) {
     m.decay <- data[1];
     max.m <- data[2];
     min.m <- data[3]
-    return(m.decay.func(age, m.decay, max.m, min.m))
+    return(m_decay_func(age, m.decay, max.m, min.m))
 }
 
-m.decay.sse <- function(data, age, vals) {
-    val.hat <- m.decay.optimizer(data, age);
+m_decay_sse <- function(data, age, vals) {
+    val.hat <- m_decay_optimizer(data, age);
     return(sum((vals - val.hat)^2))
 }
